ea82f58cdaf8a9893b14ecc1e6ac16bd48cfbafe
Andres Nayeem Mejia <50155815+andresnmejiaa@users.noreply.github.com>
2025-12-25 22:36:06 -0500

    removed garbage

--- blackscholes.py
+++ blackscholes.py
@@ -129,18 +129,7 @@ def __implied_volatility(market_price, S0, K, T, r, tolerance=1e-6, max_iter=100
 #Useful functions
 
 def GBM_paths(S0, sigma, t, r, mu, n_sims, n_steps):
-    """Simulates stock paths as geometric Brownian Motions
-    Inputs:
-    S0 (float): Underlying stock price at time 0
-    sigma (float): Yearly volatility
-    t (float): Time to expiration (years)
-    r (float): Risk-free interest rate
-    mu (float): Drift of log-returns
-    n_sims (int): Number of simulated paths
-    n_steps (int): Number of steps in each simulated path, each step interval has length t/n_steps
     
-    Return (np.array): Array of stock paths
-    """
     
     dt = t/n_steps
     noise = np.random.normal(loc = 0, scale = 1, size = (n_sims, n_steps))
@@ -150,60 +139,3 @@ def GBM_paths(S0, sigma, t, r, mu, n_sims, n_steps):
     paths_with_start = np.insert(paths, 0, S0, axis = 1)
 
     return paths_with_start
-
-def european_option_binomial(S, K, T, r, sigma, N, option_type='call'):
-    #this does nothing
-    '''
-    dt = T / N
-    u = np.exp(sigma * np.sqrt(dt)) #approximate (1+sigma*sqrt(dt)/N)^N
-    d = 1 / u
-    p = (np.exp(r * dt) - d) / (u - d) #should be 
-    discount_factor = np.exp(-r * dt)
-    possible_prices = S*(u**np.arange(N + 1)) * (d**np.arange(N-1,-1,-1))
-    
-    # Compute terminal payoff vectorized
-    if option_type == 'call':
-        payoff = np.maximum(possible_prices - K, 0)
-    elif option_type == 'put':
-        payoff = np.maximum(K - possible_prices, 0)
-    else:
-        raise ValueError("option_type must be 'call' or 'put'")
-    '''
-    return
-    
-
-def american_option_binomial(S, K, T, r, sigma, N, option_type='put'):
-    if option_type=='call':
-        return european_option_binomial
-    dt = T / N
-    u = np.exp(sigma * np.sqrt(dt)) #approximate (1+sigma*sqrt(dt)/N)^N
-    d = 1 / u
-    p = (np.exp(r * dt) - d) / (u - d) #should be 
-    q=1-p
-    discount_factor = np.exp(-r * dt)
-    possible_prices = np.zeros((N+1,N+1)) #times by state
-    for time in range(N+1):
-        for j in range(time+1):
-            possible_prices[time,j]=S*u**(time-j)*d**j
-    
-    payoff_matrix=np.zeros((N+1,N+1))
-
-    
-    if option_type == 'put':
-        payoff_matrix[N,:] = np.maximum(K-possible_prices[N,:], 0)
-    else:
-        raise ValueError("option_type must be 'call' or 'put'")
-    
-
-    
-    for time in range(N-1,-1,-1):
-        current_states=slice(0,time+1) # future times
-        #price valuations for future times from each state
-        if_wait=discount_factor * (p * payoff_matrix[time+1:current_states] + q*payoff_matrix[time+1:current_states])
-        early_exercise=np.maximum(K-possible_prices[time,current_states],0)
-        
-        payoff_matrix[time,current_states]=np.maximum(if_wait,early_exercise)
-    
-    
-    
-
